3afc10ccad4356c06f42e0275f1419a48b4fde80
Kirill12a <kirilldrozd12@gmail.com>
2021-11-02 01:50:43 +0300

    Initial Commit

---
+++ MVC#1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC#1
+//
+//  Created by Kirill Drozdov on 02.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
